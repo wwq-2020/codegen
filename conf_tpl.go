c1608129e8d304b7dbd7284293d2d7d933842766
wwq <2993895632@qq.com>
2021-09-01 15:13:34 +0800

    feat: add tracing conf

--- conf_tpl.go
+++ conf_tpl.go
@@ -1,5 +1,5 @@
 package main
 
 const (
-	confTpl = "[server]\naddr= \"127.0.0.1:8080\""
+	confTpl = "[server]\naddr= \"127.0.0.1:8080\"\n[tracing]endpoint=\"http://127.0.0.1:14268/api/traces?format=jaeger.thrift\"\nsample_rate=1.0\n"
 )
